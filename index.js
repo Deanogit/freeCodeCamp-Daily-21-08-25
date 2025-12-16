42eb7559cdb35a3ac625664ef5ce7105eaeb0ebb
Dean Howe <hideanhowe@gmail.com>
2025-12-16 09:56:59 +0000

    testing

--- index.js
+++ index.js
@@ -2,3 +2,25 @@
 // Given a number of miles ran, and a time in "MM:SS" (minutes:seconds) it took to run those miles, return a string for the average time it took to run each mile in the format "MM:SS".
 
 // Add leading zeros when needed.
+
+function milePace(miles, duration) {
+  console.log(miles, duration);
+  // divide duration by miles
+  // how do I handle the minutes and seconds?
+  // turn minutes into seconds
+  // split the duration string into an array
+  const dur = duration.split(':');
+  console.log(dur);
+  // do I need to make them a number?
+  let seconds = Number.parseInt(dur[0]) * 60 + Number.parseInt(dur[1]);
+  console.log(seconds);
+  let div = seconds / miles;
+  console.log(div / 60);
+  let minResult = div / 60;
+  let secResult = (div / 60) % 60 === 0;
+  console.log(minResult, secResult);
+  //return miles;
+  return `${minResult < 10 ? '0' + minResult : minResult}:${
+    secResult === 0 ? '00' : secResult < 10 ? '0' + secResult : secResult
+  }`;
+}
